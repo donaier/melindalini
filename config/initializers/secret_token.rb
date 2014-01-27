564ac8cea8254472033875d4e80ae7c9564a4bd4
Donat Baier <donat.baier@gmail.com>
2014-01-27 22:46:23 +0100

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Melindalini::Application.config.secret_token = '4f5a17272c5d4aa50a5485d2a99ee2366f9538bad39a83f67d9235471ddab665b1ed5407c1cb2e2a9aa997fb437bf766071a139de66585d0a17c6ce0cf4e0509'
